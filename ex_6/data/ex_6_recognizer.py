74e7ea91bc2c7a74fc1e4b12a14b5a51ae191077
Yura Vash <yuravashuk@gmail.com>
2023-05-19 11:19:42 +0300

    add ex_6

---
+++ ex_6/data/ex_6_recognizer.py
@@ -0,0 +1,33 @@
+import numpy as np
+import librosa
+import joblib
+
+def extract_features(file_path):
+    y, sr = librosa.load(file_path)
+    mel = librosa.feature.melspectrogram(y=y, sr=sr)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr)
+    features = np.hstack((np.mean(mel), np.mean(mfcc)))
+    return features
+
+def classify_audio(file_path, model_path='model.joblib', encoder_path='encoder.joblib'):
+    # Load the model and encoder
+    model = joblib.load(model_path)
+    encoder = joblib.load(encoder_path)
+
+    # Extract features from the audio file
+    features = extract_features(file_path)
+
+    # Make a prediction
+    prediction = model.predict([features])
+
+    # Decode the prediction
+    label = encoder.inverse_transform(prediction)
+
+    return label
+
+# Usage:
+file_path = './ex_6/data/testing/notepad_test.wav'
+model_path = './ex_6/data/models/model.joblib'
+encoder_path = './ex_6/data/models/encoder.joblib'
+
+print(classify_audio(file_path=file_path, model_path=model_path, encoder_path=encoder_path))
---
+++ ex_6/data/ex_6_training.py
@@ -0,0 +1,53 @@
+import os
+import numpy as np
+import librosa
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+import joblib
+
+# Directory where your .wav files are organized in subfolders
+main_directory = './ex_6/data/training/'
+
+# List for features and labels
+features = []
+labels = []
+
+# Iterate over subfolders in main directory
+for subfolder in os.listdir(main_directory):
+    subfolder_path = os.path.join(main_directory, subfolder)
+    # Only process subfolders (skip any file that might be in the main directory)
+    if os.path.isdir(subfolder_path):
+        # Iterate over files in subfolder
+        for filename in os.listdir(subfolder_path):
+            if filename.endswith(".wav"):
+                # Load .wav file
+                y, sr = librosa.load(os.path.join(subfolder_path, filename))
+
+                # Extract MEL spectrogram
+                mel = librosa.feature.melspectrogram(y=y, sr=sr)
+
+                # Extract MFCC (mel-frequency cepstral coefficients)
+                mfcc = librosa.feature.mfcc(y=y, sr=sr)
+
+                # Append features to the feature list
+                features.append(np.hstack((np.mean(mel), np.mean(mfcc))))
+
+                # Append the label (subfolder name) to the labels list
+                labels.append(subfolder)
+                print(subfolder)
+
+# Encode labels
+encoder = LabelEncoder()
+labels = encoder.fit_transform(labels)
+
+# Split dataset into training and test set
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.4)
+
+# Train model
+model = RandomForestClassifier()
+model.fit(features_train, labels_train)
+
+# Save model and encoder
+joblib.dump(model, 'ex_6/data/models/model.joblib')
+joblib.dump(encoder, 'ex_6/data/models/encoder.joblib')
